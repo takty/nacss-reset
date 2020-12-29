cd0c770114a08f2fe19bd201eabb9f69a1ee13a4
takty <tokutadaginaya@gmail.com>
2020-12-29 23:53:19 +0900

    Refactor gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,16 @@ const SASS_OUTPUT_STYLE = 'compressed';  // 'expanded' or 'compressed'
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')({ pattern: ['gulp-*'] });
 
+const plumberOptions = {
+	errorHandler: function (err) {
+		console.log(err.messageFormatted);
+		this.emit('end');
+	}
+};
+
 
 gulp.task('sass', () => gulp.src(['src/**/*.scss'])
-	.pipe($.plumber({
-		errorHandler: function (err) {
-			console.log(err.messageFormatted);
-			this.emit('end');
-		}
-	}))
+	.pipe($.plumber(plumberOptions))
 	.pipe($.sourcemaps.init())
 	.pipe($.dartSass({ outputStyle: SASS_OUTPUT_STYLE }))
 	.pipe($.autoprefixer())
